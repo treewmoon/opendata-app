4968b3e4e3c8001191a0bf7f8a9965ed1cfe172a
treewmoon <tanahashi.k.aa@gmail.com>
2018-11-27 02:30:22 +0900

    見やすく改行

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,7 +3,11 @@ class RecordsController < ApplicationController
   end
 
   def create
-      record = Record.create(start_station_id: record_create_params[:start_station_id],goal_station_id: record_create_params[:goal_station_id],opponent: record_create_params[:opponent],user_id:current_user.id)
+      record = Record.create(\
+        start_station_id: record_create_params[:start_station_id],\
+        goal_station_id: record_create_params[:goal_station_id],\
+        opponent: record_create_params[:opponent],\
+        user_id:current_user.id)
       redirect_to "/records/#{record.id}/edit"
   end
 
@@ -13,7 +17,10 @@ class RecordsController < ApplicationController
 
   def update
     record = Record.find(params[:id])
-    record.update(consumed_calory: record_update_params[:consumed_calory],running_time: record_update_params[:running_time],result: record_update_params[:result])
+    record.update(\
+      consumed_calory: record_update_params[:consumed_calory],\
+      running_time: record_update_params[:running_time],\
+      result: record_update_params[:result])
     redirect_to "/records/#{record.id}"
   end
 
